5a9517b1eca50756c621bf3e758b953b25deed80
SRUJANGOWDA01 <srujangowda46@gmail.com>
2024-02-07 20:41:03 +0530

    zlib

---
+++ modules/zlib/p1.js
@@ -0,0 +1,13 @@
+const zlib = require('zlib')
+const fs = require('fs')
+
+let gzip = zlib.createGzip()
+
+let r = fs.createReadStream('./myfile.txt')
+let w = fs.createWriteStream('test.gz')
+
+r.pipe(gzip).pipe(w)
+
+console.log(`zip file created`)
+
+/* zip file created */
